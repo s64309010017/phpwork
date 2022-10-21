df50301f6c6a0a913e59f15ad393d4d53b077ce3
s64309010017 <s64309010017@ptnicec.ac.th>
2022-10-21 11:50:29 +0700

    comment.php

---
+++ comment.php
@@ -0,0 +1,24 @@
+<?php
+
+// [บรรทัดที่ 6-7 แสดงผลการใช้งาน Comment แบบบรรทัดเดียว
+echo "Rusnee pohma<br>";
+echo "Information Technology<br>";
+//echo "Pattani<br>";
+#echo "Thailand";
+
+//บรรทัดที่10-12 แสดงผลการใช้งาน Comment แบบหลายบรรทัด
+/*echo "<br>";
+echo "94000 <br>";
+echo "Asia <br>";*/
+echo "Universal";
+
+
+
+
+
+
+
+
+
+
+?>
