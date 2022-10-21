cdbacdbcf8ac7a55a0bb2b4ecbf2d558dc401812
s64309010017 <s64309010017@ptnicec.ac.th>
2022-10-21 11:28:51 +0700

    ส่งไฟล์ test ครั้งที่1

--- test.php
+++ test.php
@@ -1,3 +1,23 @@
 <?php
     echo "Rusnee pohma";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEST</title>
+</head>
+<body>
+   <br> Information Technology <br>
+   <?php
+       echo "pattani";
+   ?>
+
+</body>
+</html>
+<?php
+
+   echo " Thailand";
+?>
